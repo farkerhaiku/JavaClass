0dc97c0d389fd692c7190fe21ebac51045210461
Jason Mayer <jason.mayer@nytimes.com>
2017-07-03 17:46:29 -0400

    adding placeholder list and display text

--- app/src/main/java/edu/stlcc/jasonmayer/javaclass/activity/ProjectTwoActivity.java
+++ app/src/main/java/edu/stlcc/jasonmayer/javaclass/activity/ProjectTwoActivity.java
@@ -6,12 +6,25 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SwitchCompat;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import edu.stlcc.jasonmayer.javaclass.R;
+import edu.stlcc.jasonmayer.javaclass.models.Person;
 
 public class ProjectTwoActivity extends AppCompatActivity {
+
+    private SwitchCompat newUserTypeToggle;
+    private ListView listView;
+    private List<Person> customers = new ArrayList<>();
+    private List<Person> employees = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +33,13 @@ public class ProjectTwoActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        newUserTypeToggle = (SwitchCompat) findViewById(R.id.customer_employee_toggle);
+        listView = (ListView) findViewById(R.id.listView);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
             }
         });
     }
