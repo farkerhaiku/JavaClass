3b8fa1e405279f8fb9d49aed18137568779a3548
Jason Mayer <jason.mayer@nytimes.com>
2017-07-05 21:31:47 -0400

    removing default fab

--- app/src/main/java/edu/stlcc/jasonmayer/javaclass/activity/ProjectThreeActivity.java
+++ app/src/main/java/edu/stlcc/jasonmayer/javaclass/activity/ProjectThreeActivity.java
@@ -19,15 +19,6 @@ public class ProjectThreeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_project_three);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     public static Intent getLaunchIntent(Activity activity) {
